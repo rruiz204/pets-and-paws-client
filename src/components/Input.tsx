9a9c7f4e0e0bc687f289a39b24ef5bb3cad39b10
roney ruiz <roneyruizrojas@gmail.com>
2024-09-13 21:10:29 -0600

    undestand how input component works

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,21 +1,21 @@
 import { FieldValues, UseFormRegister, Path } from "react-hook-form";
 import Capitalize from "../utils/Capitalize";
 
-interface Props<T extends FieldValues> {
-  label: Path<T>;
+interface Props<Inputs extends FieldValues> {
+  label: Path<Inputs>;
+  register: UseFormRegister<Inputs>
   placeholder: string;
   type: string;
-  register: UseFormRegister<T>;
   error?: string;
-};
+}
 
-function Input<T extends FieldValues>({ label, placeholder, type, register, error } : Props<T>) {
+function Input<Inputs extends FieldValues>
+  ({ label, type, placeholder, error, register }: Props<Inputs>) {
   return (
     <div className="flex flex-col">
       <label className="font-semibold mb-1">{Capitalize(label)}</label>
-      <input type={type} placeholder={placeholder} {...register(label)}
-        className="outline-none border px-3 py-2 rounded-md" />
-      {error && <p>{Capitalize(error)}</p>}
+      <input type={type} placeholder={placeholder} {...register(label)} />
+      { error && <p>{Capitalize(error)}</p> }
     </div>
   );
 };
