9cf12ae2c1c8a10dd5da7a97eda64d764d17158d
roney ruiz <roneyruizrojas@gmail.com>
2024-09-16 11:49:17 -0600

    work in login view

--- src/modules/login/Form.tsx
+++ src/modules/login/Form.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { schema, FormInputs } from "./validation";
-import Input from "../../components/Input";
+import Field from "../../components/Field";
+import Button from "../../components/Button";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 function Form() {
@@ -11,12 +12,14 @@ function Form() {
 
   return (
     <div>
-      <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
-        <Input label="email" placeholder="example@gmail.com" type="text"
-          register={register} error={errors.email?.message}></Input>
-        <Input label="password" placeholder="password123" type="password"
-          register={register} error={errors.password?.message}></Input>
-        <button>Submit</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-3 mb-6">
+          <Field label="email" placeholder="example@gmail.com" type="text"
+            register={register} error={errors.email?.message}></Field>
+          <Field label="password" placeholder="password123" type="password"
+            register={register} error={errors.password?.message}></Field>
+        </div>
+        <Button text="Login"></Button>
       </form>
     </div>
   );
--- src/modules/login/Login.tsx
+++ src/modules/login/Login.tsx
@@ -1,15 +1,14 @@
 import Form from "./Form";
-import PawSvg from "../../assets/svgs/paw.svg";
+import Logo from "../../assets/imgs/pets-and-paws-logo.png"
 
 function Login() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="p-2 min-w-80 w-[450px]">
-        <div className="">
-          <img src={PawSvg} alt="Paw" className="block mx-auto" width={100} height={100}/>
+    <div className="w-screen h-screen flex justify-center pt-24 text-seconday">
+      <div className="p-4 min-w-80 w-[450px]">
+        <div className="mb-8">
+          <img src={Logo} alt="Paw" className="block mx-auto" width={250} height={250}/>
           <h1 className="text-center text-3xl font-bold">Sign in to your account</h1>
         </div>
-        <br />
         <div className="border-2 p-10 rounded-md">
           <Form></Form>
         </div>
