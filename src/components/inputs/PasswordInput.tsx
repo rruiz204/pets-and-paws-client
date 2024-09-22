1e8151c387757d1d1ad6a5ae9f4bf81ffb7c6515
roney ruiz <roneyruizrojas@gmail.com>
2024-09-21 18:06:46 -0600

    finish password input

--- src/components/inputs/PasswordInput.tsx
+++ src/components/inputs/PasswordInput.tsx
@@ -1,7 +1,6 @@
 import { FieldValues } from "react-hook-form";
 import { useState } from "react";
 import { FieldProps, InputProps } from "../forms/Field";
-import SimpleInput from "./SimpleInput";
 import EyeIcon from "../../assets/svgs/ege.svg";
 import SlashEyeIcon from "../../assets/svgs/slash-eye.svg";
 
@@ -13,17 +12,20 @@ function PasswordInput<Values extends FieldValues>({ placeholder, register, path
   const toggleHidden = () => setHidden(!hidden);
 
   return (
-    <div className="flex items-center">
+    <div className="border-2 flex items-center rounded-md p-1">
       <div className="w-[90%]">
-        <SimpleInput type={hidden ? "password" : "text"} path={path} placeholder={placeholder} register={register}></SimpleInput>
+        <input type={hidden ? "password" : "text"} placeholder={placeholder} {...register(path)} 
+          className="outline-none p-1 w-full"/>
       </div>
-      <button onClick={toggleHidden} type="button" className="border-2">
-        {
-          hidden
-            ? <img src={EyeIcon} width={20} height={20} />
-            : <img src={SlashEyeIcon} width={20} height={20} />
-        }
-      </button>
+      <div className="flex justify-center w-[10%]">
+        <button onClick={toggleHidden} type="button" className="p-1 rounded-lg hover:bg-[#ebebeb] duration-200">
+          {
+            hidden
+              ? <img src={EyeIcon} width={20} height={20} />
+              : <img src={SlashEyeIcon} width={20} height={20} />
+          }
+        </button>
+      </div>
     </div>
   );
 };
