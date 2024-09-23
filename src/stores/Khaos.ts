08b411d9b67ee8d0e959eff48e8fe180768a6867
roney ruiz <roneyruizrojas@gmail.com>
2024-09-23 11:54:04 -0600

    create khaos to fetch data

---
+++ src/stores/Khaos.ts
@@ -0,0 +1,38 @@
+import { BASE_URL } from "../env";
+
+type HttpMethod = "GET" | "POST";
+
+interface HttpResponse<Dto> {
+  data?: Dto;
+  error?: string;
+};
+
+class Khaos {
+  private url: string = BASE_URL;
+
+  private options: RequestInit = {
+    method: "GET",
+    headers: { "Content-Type": "application/json" }
+  };
+
+  constructor(endpoint: string) {
+    this.url += endpoint;
+  };
+
+  public setHttpMethod(method: HttpMethod) {
+    this.options.method = method;
+    return this;
+  };
+
+  public setBody(body: any) {
+    this.options.body = JSON.stringify(body);
+    return this;
+  };
+
+  public async fetcher<Dto>(): Promise<HttpResponse<Dto>> {
+    const response = await fetch(this.url, this.options);
+    return (await response.json()) as HttpResponse<Dto>;
+  };
+};
+
+export default Khaos;
