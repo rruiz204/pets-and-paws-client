ef947fc54941cccc66571e8df696f5c083855f1a
roney ruiz <roneyruizrojas@gmail.com>
2024-09-28 21:23:26 -0600

    rename from dashboard to module

---
+++ src/modules/home/Home.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "../../components/navigation/Sidebar";
+
+function Home() {
+  return (
+    <div className="flex">
+      <Sidebar></Sidebar>
+    </div>
+  );
+};
+
+export default Home
