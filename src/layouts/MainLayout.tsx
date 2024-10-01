19cf11fd1a019c21c34a88b60efb6998329d8470
roney ruiz <roneyruizrojas@gmail.com>
2024-10-01 14:06:12 -0600

    build topbar and implement it in the layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/navigation/Sidebar";
+import Topbar from "../components/navigation/Topbar";
 
 function MainLayout() {
   return (
-    <div className="flex">
-      <Sidebar></Sidebar>
+    <div className="flex flex-col md:flex-row">
+      <div>
+        <div className="hidden md:block">
+          <Sidebar></Sidebar>
+        </div>
+        <div className="md:hidden">
+          <Topbar></Topbar>
+        </div>
+      </div>
       <div className="content">
         <Outlet></Outlet>
       </div>
