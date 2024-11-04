65c09abdc15b4c5ea6ed888bfe3625cb53ea0ed9
roney ruiz <roneyruizrojas@gmail.com>
2024-11-04 16:55:28 -0600

    feat: work on new main layout

---
+++ src/core/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "@shared/components/navigation/Sidebar";
+
+function MainLayout() {
+  return (
+    <div className="flex">
+      <div>
+        <Sidebar></Sidebar>
+      </div>
+
+      <div className="content flex-1 px-8 py-6">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
