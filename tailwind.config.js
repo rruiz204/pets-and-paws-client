035b5078c822823372bd79f4ce825aacd143190b
roney ruiz <roneyruizrojas@gmail.com>
2024-11-04 20:42:32 -0600

    chore: add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#B7323B",
-        seconday: "#25242E",
-        tertiary: "#862D36",
-        "hover-beige": "#EBEBEB"
-      }
+        "cs-red-100": "#B7323B",
+        "cs-red-200": "#862d36",
+        "cs-red-300": "#552832",
+        "cs-blue-300": "#25242E",
+        "cs-white-100": "#FCFCFC",
+        "cs-white-200": "#F9F9F9",
+        "cs-white-300": "#EBEBEB",
+      },
     },
   },
   plugins: [],
