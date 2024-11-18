4065176fdd681af95d4e4ee015b767bee7cb51cc
roney ruiz <roneyruizrojas@gmail.com>
2024-11-18 16:20:17 -0600

    feat: add new modules

--- src/core/routes/Privates.tsx
+++ src/core/routes/Privates.tsx
@@ -1,10 +1,20 @@
 import { RouteObject } from "react-router-dom";
 import MainLayout from "@core/layouts/MainLayout";
 import Home from "@modules/home/Home";
+import Directory from "@modules/pets/directory";
+import Profile from "@modules/profile/me";
 
 const privates: RouteObject[] = [
   { path: "/", element: <MainLayout/>, children: [
     { path: "/home", element: <Home/> },
+    
+    { path: "/pets", children: [
+      { path: "directory", element: <Directory/> }
+    ]},
+    
+    { path: "/profile", children: [
+      { path: "me", element: <Profile/> }
+    ]},
   ]}
 ];
 
---
+++ src/modules/pets/directory/index.tsx
@@ -0,0 +1,9 @@
+function Directory() {
+  return (
+    <div>
+      <p>pets directory works!</p>
+    </div>
+  );
+};
+
+export default Directory;
